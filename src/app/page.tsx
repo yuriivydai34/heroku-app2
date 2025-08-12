8b8b06859fd2f3d30115f5ee89cd40a108ec5e89
Yurii Vydai <yuriivydai@gmail.com>
2025-08-12 14:52:40 +0300

    register form, post users api request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Suspense } from 'react';
 import Profile from './Profile';
+import Register from './Register';
 
 export default function Home() {
   return (
-    <div>
-      <Suspense fallback={'Loading profile...'}>{<Profile />}</Suspense>
+    <div className="min-h-screen bg-gray-100 py-8">
+      <div className="container mx-auto px-4">
+        <Register />
+        <div className="mt-8">
+          <Suspense fallback={'Loading profile...'}>{<Profile />}</Suspense>
+        </div>
+      </div>
     </div>
   );
 }
