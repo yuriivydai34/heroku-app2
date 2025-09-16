745a308b608101adff50643c020ae57157663f65
Yurii Vydai <yuriivydai@gmail.com>
2025-09-16 20:57:49 +0300

    using auth guard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import AuthGuard from "@/components/AuthGuard";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          {children}
+          <AuthGuard>{children}</AuthGuard>
         </Providers>
       </body>
     </html>
